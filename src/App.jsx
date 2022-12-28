3785fdac9b02a1ffe33780d6e14d062be082f06c
Elelia <llucas@ensup.eu>
2022-12-28 18:08:37 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import axios from "axios";
 import Connexion from "./components/connexion/connexion_page";
 
 function App() {
@@ -7,3 +9,25 @@ function App() {
 }
 
 export default App;
+
+// const baseURL = "http://localhost:5000/users";
+//
+// export default function App() {
+//   const [post, setPost] = React.useState(null);
+//
+//   React.useEffect(() => {
+//     axios.get(baseURL).then((response) => {
+//       setPost(response.data);
+//       console.log('super ?');
+//     });
+//   }, []);
+//
+//   if (!post) return null;
+//
+//   return (
+//       <div>
+//         <h1>{post.title}</h1>
+//         <p>{post.body}</p>
+//       </div>
+//   );
+// }
--- src/components/connexion/connexion_page.jsx
+++ src/components/connexion/connexion_page.jsx
@@ -1,40 +1,87 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
+import axios from "axios";
 import './connexion_page.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class Connexion extends Component {
-    render () {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-4"></div>
-                    <div className="col-4">
-                        <h1 className="maintitle">Mark</h1>
-                        <div className="card">
-                            <div className="card-body">
-                                <h2>Connectez-vous !</h2>
-                                <form method="post">
-                                    <div className="input-group mb-3">
-                                        <input type="text" className="form-control" placeholder="Email" aria-label="Email"/>
-                                    </div>
-                                    <div className="input-group mb-3">
-                                        <input type="text" className="form-control" placeholder="Mot de passe" aria-label="Mot de passe"/>
-                                    </div>
-                                    <button type="submit" className="">Connexion</button>
-                                </form>
-                            </div>
-                        </div><br/>
-                        <div className="card">
-                            <div className="card-body">
-                                <h2>Pas encore inscrit ?</h2>
-                                <button type="button" className="">Inscription</button>
-                            </div>
+const baseURL = "http://localhost:5000/users";
+
+export default function LoginTest() {
+    const [users, setUser] = useState([]);
+
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            setUser(response.data);
+            console.log(response);
+        });
+    }, []);
+
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-4"></div>
+                <div className="col-4">
+                    <h1 className="maintitle">Mark</h1>
+                    <div className="card">
+                        <div className="card-body">
+                            <h2>Connectez-vous !</h2>
+                            <form method="post">
+                                <div className="input-group mb-3">
+                                    <input type="text" className="form-control" placeholder="Email" aria-label="Email"/>
+                                </div>
+                                <div className="input-group mb-3">
+                                    <input type="password" className="form-control" placeholder="Mot de passe" aria-label="Mot de passe"/>
+                                </div>
+                                <button type="submit" className="">Connexion</button>
+                            </form>
+                        </div>
+                    </div><br/>
+                    <div className="card">
+                        <div className="card-body">
+                            <h2>Pas encore inscrit ?</h2>
+                            <button type="button" className="">Inscription</button>
                         </div>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    )
 }
 
-export default Connexion;
+// class Connexion extends Component {
+//
+//     render () {
+//         return (
+//             <div className="container">
+//                 <div className="row">
+//                     <div className="col-4"></div>
+//                     <div className="col-4">
+//                         <h1 className="maintitle">Mark</h1>
+//                         <div className="card">
+//                             <div className="card-body">
+//                                 <h2>Connectez-vous !</h2>
+//                                 <form method="post">
+//                                     <div className="input-group mb-3">
+//                                         <input type="text" className="form-control" placeholder="Email" aria-label="Email"/>
+//                                     </div>
+//                                     <div className="input-group mb-3">
+//                                         <input type="password" className="form-control" placeholder="Mot de passe" aria-label="Mot de passe"/>
+//                                     </div>
+//                                     <button type="submit" className="">Connexion</button>
+//                                 </form>
+//                             </div>
+//                         </div><br/>
+//                         <div className="card">
+//                             <div className="card-body">
+//                                 <h2>Pas encore inscrit ?</h2>
+//                                 <button type="button" className="">Inscription</button>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+// export default Connexion;
+//export default LoginTest();
