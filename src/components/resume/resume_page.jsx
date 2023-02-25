70c552489d649e7a129776628311c6918541f339
Lisa <llucas@ensup.eu>
2023-02-25 16:01:40 +0100

    update

--- src/components/resume/resume_page.jsx
+++ src/components/resume/resume_page.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
+import { BsFillPlayCircleFill } from "react-icons/bs";
+import ReactPlayer from 'react-player';
 
 export default function ResumePage({isOpen, closeModal, video}) {
+    const [videoModalIsOpen, setVideoModalIsOpen] = useState(false);
 
     return (
         <Modal
@@ -9,10 +12,42 @@ export default function ResumePage({isOpen, closeModal, video}) {
             onRequestClose={closeModal}
             contentLabel="Video Modal"
         >
-            <h2>{video.nom}</h2>
-            <p>{video.resume}</p>
-            <p>{video.annee}</p>
+            <div className="container">
+                <div className="row">
+                    <div className="col-8">
+                        <h2>{video.nom}</h2>
+                        <p>{video.resume}</p>
+                        <p>{video.annee}</p>
+                        <span onClick={() => setVideoModalIsOpen(true)}><BsFillPlayCircleFill size={32}/></span>
+                    </div>
+                    <div className="col-4">
+                        <p>Là ce serait les avis</p>
+                    </div>
+                </div>
+            </div>
             <button onClick={closeModal}>Close</button>
+            <Modal
+                isOpen={videoModalIsOpen}
+                onRequestClose={() => setVideoModalIsOpen(false)}
+                contentLabel="Video Player Modal"
+                style={{
+                    overlay: {
+                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                    },
+                    content: {
+                        top: '50%',
+                        left: '50%',
+                        right: 'auto',
+                        bottom: 'auto',
+                        marginRight: '-50%',
+                        transform: 'translate(-50%, -50%)',
+                        maxWidth: '1100px',
+                        maxHeight: '900px',
+                    },
+                }}
+            >
+                <ReactPlayer url="https://www.youtube.com/watch?v=mqqft2x_Aa4" playing={videoModalIsOpen} />
+            </Modal>
         </Modal>
     );
 }
